53c93b88826d20c617ad5ce903819cc9b10c2d8c
coulc <542587642@qq.com>
2025-06-02 12:01:42 +0000

    "seven"

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -1,4 +1,18 @@
 'use strict';
+/*
+    Author:   coulc 
+    Date:     2025/5/30
+
+    study:
+        Conversion,Parsing    isNaN() parseInt() parseFloat() isFinite() isInteger()
+        Math and Rounding      sqrt() floor() trunc() random() toFixed() ceil()
+        numeric separators   _
+        BigInt     BigInt()   n   Number.MAX_SAFE_INTEGER
+        Date   Date()   getFullYear()  getMonth() getDate() getDay() getMinutes() getSeconds() toISOString() getTime() now()
+        Intl.DateTimeFormat()  Intl.NumberFormat()
+        setTimeout() clearTimeout() setInterval()
+
+*/
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -21,12 +35,12 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2020-07-26T17:01:17.194Z',
+    '2020-07-28T23:36:17.929Z',
+    '2020-08-01T10:51:36.790Z',
   ],
   currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+  locale: 'en-US', // de-DE
 };
 
 const account2 = {
@@ -81,20 +95,59 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
-  containerMovements.innerHTML = '';
+const formatMovementDate = function (date, locale) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  // const day = `${date.getDate()}`.padStart(2, 0);
+  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  // const year = date.getFullYear();
+  // return `${day}/${month}/${year}`;
+  return new Intl.DateTimeFormat(locale).format(date);
+};
+
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+
+const displayMovements = function (acc, sort = false) {
+    containerMovements.innerHTML = '';
+
+    const combinedMovsDates = acc.movements.map((mov, i) => ({
+        movement: mov,
+        movementDate: acc.movementsDates.at(i),
+    }));
+
+    if (sort) combinedMovsDates.sort((a, b) => a.movement - b.movement);
+    
+
+
+  combinedMovsDates.forEach(function (obj, i) {
+    const { movement, movementDate } = obj;
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(movementDate);
+    const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formatCur(movement, acc.locale, acc.currency);
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
@@ -104,19 +157,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +179,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
@@ -142,7 +195,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -151,9 +204,43 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    // In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = 'Log in to get started';
+      containerApp.style.opacity = 0;
+    }
+
+    // Decrease 1s
+    time--;
+  };
+
+  // Set time to 5 minutes
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  return timer;
+};
+
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
+
+// FAKE ALWAYS LOGGED IN
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -164,16 +251,45 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
 
+    // Create current date and time
+    const now = new Date();
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      // weekday: 'long',
+    };
+    // const locale = navigator.language;
+    // console.log(locale);
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+
+    // const day = `${now.getDate()}`.padStart(2, 0);
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    // const year = now.getFullYear();
+    // const hour = `${now.getHours()}`.padStart(2, 0);
+    // const min = `${now.getMinutes()}`.padStart(2, 0);
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
+
+    // Timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
 
     // Update UI
     updateUI(currentAccount);
@@ -182,7 +298,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -198,22 +314,39 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    // Add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Update UI
-    updateUI(currentAccount);
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
+
+      // Update UI
+      updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -223,7 +356,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -244,10 +377,242 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  // BUG in video:
+  // displayMovements(currentAccount.movements, !sorted);
+
+  // FIX:
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+
+/* Conversion and Checking Numbers 
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+console.log(Number('23'));
+console.log(+'23');
+
+
+console.log(Number.parseInt('20px',10));
+console.log(Number.parseInt('e123',10));
+
+console.log(Number.parseInt('   2.5rem   '));
+console.log(Number.parseFloat('   2.5rem     '));
+console.log(parseFloat('   2.5rem     '));
+
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
+
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23.123));
+console.log(Number.isInteger(23 / 0));
+
+*/
+
+/*Math and Rounding 
+
+console.log(Math.sqrt(25))
+console.log(Math.PI * Number.parseFloat('10px') ** 2)
+
+console.log(Math.trunc(Math.random() * 6) + 1)
+
+const randomInt = (min,max) => Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(10,20))
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.9));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+
+console.log(Math.trunc(23.9))
+
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
+
+*/
+
+/* The Remainder Operator
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 / 3);
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(123));
+console.log(isEven(2));
+console.log(isEven(1));
+console.log(isEven(234));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+
+*/
+
+
+/*  numeric separators
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+// ERROR
+// const PI = 3._123;
+// const PI = 3_.123;
+// const PI = 3.1__23;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'))
+console.log(parseInt('230_000'))
+
+*/
+
+
+/* BigInt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+console.log(2 ** 53 + 5);
+console.log(2 ** 53 + 6);
+
+console.log(34986723086092358690234586908345906830495678n);
+console.log(BigInt(34986723086092358690234586908345906830495678));
+console.log(100n + 100n);
+
+console.log(23590132478602384609823096n+29350932485092380954n)
+
+const huge = 203958093485902345n;
+const num = 100;
+console.log(huge * BigInt(num));
+
+console.log(20n > 15);
+console.log(20n === 20); // false
+console.log(typeof 20n);
+console.log(20n == '20'); // true
+console.log(20n == 20); // true
+
+// console.log(Math.sqrt(huge));  // ERROR
+console.log(10n / 3n);
+console.log(11n / 3n);
+
+*/
+
+/* Date 
+
+
+const now = new Date(); 
+console.log(now);
+
+console.log(new Date('Aug 02 2025 09:02:11'));
+console.log(new Date('December 24,2025 '));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037,10,19,15,23,5));
+console.log(new Date(2037,10,31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+
+const future = new Date(2026,10,19,15,23);
+console.log(future);
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+
+console.log(future.getTime());
+
+console.log(new Date(12312464));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
+/*
+const future = new Date(2037,10,19,15,23);
+console.log(+future);
+const calcDaysPassed = (data1,data2) => Math.abs(data2 - data1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037,3,4),new Date(2037,3,14));
+console.log(days1);
+*/
+
+/*
+const num = 123123.124;
+console.log(new Intl.NumberFormat('en-US').format(num))
+const options = {
+    style: 'currency',
+    unit: 'celsius',
+    currency: 'EUR',
+}
+console.log(
+    navigator.language,
+    new Intl.NumberFormat(navigator.language,options).format(num)
+)
+*/
+/*
+const ingredients = ['olives','spinach'];
+const pizzaTimer = setTimeout(
+    (ing1,ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
+    3000,
+    ...ingredients,
+);
+console.log('Waitting...');
+if(ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+setInterval(() => {
+    const now = new Date();
+    console.log(now);
+},1000);
+
+*/
+
+
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/*
+    Author:   coulc 
+    Date:     2025/5/31
+
+    study:
+        DOM  select  create remove, prepend()  append() cloneNode() before() after() remove()
+        Attributes,Data Attributes,Classes
+        getBoundingClientRect()
+        
+*/
+
+
 ///////////////////////////////////////
 // Modal window
 
@@ -8,24 +20,254 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (e) {
+    // 防止回到顶部
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click',openModal))
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
 });
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click',(e) => {
+    const s1coords = section1.getBoundingClientRect();
+    section1.scrollIntoView({behavior:'smooth'});
+})
+
+// document.querySelectorAll('.nav__link').forEach(
+//     (e) => {
+//         e.addEventListener('click',function(e){
+//             e.preventDefault();
+//             const id = this.getAttribute('href');
+//             console.log(id);
+//             document.querySelector(id).scrollIntoView({behavior:'smooth'});
+//         })
+//     }
+// );
+
+document.querySelector('.nav__links').addEventListener('click',function(e){
+    e.preventDefault();
+    console.log(e.target);
+    if(e.target.classList.contains('nav__link')){
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior:'smooth'});
+    }
+});
+
+
+
+//////////////////////////////////////
+// LECTURE
+/*
+// Selecting elements
+console.log(document);
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSelector = document.querySelectorAll('.section');
+console.log(allSelector);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+
+// Creating  and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie--message');
+// message.textContent = `hello world,this is text button! <button class = "btn btn--close-cookie">Got it!</button>`;
+message.innerHTML = `hello world,this is text button! <button class = "btn btn--close-cookie">Got it!</button>`;
+// header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+header.after(message);
+
+// Delete elements
+document.querySelector('.btn--close-cookie')
+    .addEventListener('click', () => {
+    // message.remove();
+    message.parentElement.removeChild(message);
+});
+
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary','orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+logo.alt = 'Beautiful minimalist logo';
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company','bankist');
+console.log(logo.getAttribute('company'));
+
+const src = logo.getAttribute('src');
+console.log(src);
+
+// const link = document.querySelector('.twitter-link');
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+
+// Data Attributes
+console.log(logo.dataset.versionNumber);
+
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+
+// Don't use 
+// logo.className = 'Jonas';
+
+*/  
+
+/*
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',(e) => {
+    // 获取目标元素的位置信息
+    const s1coords = section1.getBoundingClientRect(); // 返回一个包含元素的大小及其相对于视口的位置信息的对象
+    console.log(s1coords);
+
+    // 获取触发事件的元素的位置信息
+    console.log(e.target.getBoundingClientRect());
+    // 获取当前页面的水平和垂直滚动偏移量
+    console.log('Current scroll(X/Y) ',window.pageXOffset,window.pageYOffset);
+    console.log(
+        'height/width viewport',
+        //获取视口的高度和宽度 
+        document.documentElement.clientHeight,
+        document.documentElement.clientWidth,
+    );
+
+    // scrollTO  滚动窗口到指定位置 
+    // window.scrollTo(
+    //     s1coords.left + window.pageXOffset,
+    //     s1coords.top + window.pageYOffset
+    // );
+
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffset,
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: 'smooth',
+    // });
+    
+    section1.scrollIntoView({behavior:'smooth'});
+});
+
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = (e) => {
+    alert('addEventListener:Great!  You are reading the heading :D');
+    h1.removeEventListener('mouseenter',alertH1);
+}
+
+h1.addEventListener('mouseenter',alertH1);
+
+// h1.onmouseenter = (e) => {
+//     alert('addEventListener:Great!  You are reading the heading :D');
+// }
+
+setTimeout(() => h1.removeEventListener('mouseenter',alertH1),3000);
+
+*/
+
+/*
+const randomInt = (min,max) => 
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => 
+    `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+
+console.log(randomColor());
+
+
+document.querySelector('.nav__link').addEventListener('click',function(e) {
+    console.log('click the link');
+    this.style.backgroundColor = randomColor();
+    console.log('LINK',e.terget,e.currentTarget);
+    console.log(e.currentTarget === this);
+
+    // e.stopPropagation();
+
+});
+
+document.querySelector('.nav__links').addEventListener('click',function(e) {
+    console.log('click the link');
+    this.style.backgroundColor = randomColor();
+    console.log('CONTAINER',e.terget,e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click',function(e) {
+    console.log('click the link');
+    this.style.backgroundColor = randomColor();
+    console.log('NAV',e.terget,e.currentTarget);
+},true);
+
+*/
+
+const h1 = document.querySelector('h1');
+
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+
+// DOM Travesing   
+//  太无聊 直接跳   这一章学的不太好  2025/6/2
+
+
+
